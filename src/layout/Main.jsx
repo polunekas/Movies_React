20807414adf3e6109ca253a0a9660566ecff7c11
polina <polune.kas@gmail.com>
2023-06-18 23:26:58 +0300

    feat: add API, movies cards

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,7 +1,32 @@
-function Main() {
-	return <main className='container content'>
-		Hello from Movies App
+import React from 'react'
+import {Movies} from "../components/Movies"
+
+class Main extends React.Component{
+	state = {
+		movies: [],
+	}
+
+componentDidMount() {
+	fetch("http://www.omdbapi.com/?apikey=4a6fc8e5&s=matrix")
+		.then(response => response.json())
+		.then(data => this.setState({movies: data.Search}))
+
+}
+
+
+	render() {
+		const {movies} = this.state
+
+		return <main className='container content'>
+
+			{
+				movies.length ? (
+				<Movies movies ={this.state.movies} />
+				) : <h5>Loading...</h5>
+			}
+	
 	</main>
+}
 }
 
 export {Main}
