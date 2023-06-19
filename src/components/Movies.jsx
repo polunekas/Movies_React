055b9635f96ed702ac61abae9fcc92a4abe5d687
polina <polune.kas@gmail.com>
2023-06-19 18:36:15 +0300

    feat: gh-pages add

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,15 +1,17 @@
-import {Movie} from './Movie'
+import { Movie } from './Movie';
 
 function Movies(props) {
-	const {movies} = props;
+  const { movies = [] } = props;
 
-	return (
-	<div className='movies'>
-		{movies.map(movie => (
-			<Movie key={movie.imdbID} {...movie}/>
-		))}
-	</div>
-)	
+  return (
+    <div className="movies">
+      {movies.length ? (
+        movies.map((movie) => <Movie key={movie.imdbID} {...movie} />)
+      ) : (
+        <h4>Nothing found</h4>
+      )}
+    </div>
+  );
 }
 
-export {Movies}
+export { Movies };
