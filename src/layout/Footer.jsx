a11fe3e5d1cfc7f52c08556fdd65dae7f21a915c
polina <polune.kas@gmail.com>
2023-06-20 18:12:42 +0300

    fix footer layout and add readme

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,14 +1,14 @@
 function Footer() {
-return (
-	<footer className="page-footer #e0f2f1 teal lighten-5">
-          <div className="footer-copyright">
-            <div className="container">
-            © {new Date().getFullYear()} Copyright Text
-            <a className="grey-text text-lighten-4 right" href="#!">Repo</a>
-            </div>
-          </div>
-	</footer>
-)
+  return (
+    <footer className="page-footer #e0f2f1 teal lighten-2">
+      <div className="container">
+        © {new Date().getFullYear()} Copyright Text
+        <a className="grey-text text-lighten-4 right" href="#!">
+          Repo
+        </a>
+      </div>
+    </footer>
+  );
 }
 
-export {Footer}
+export { Footer };
